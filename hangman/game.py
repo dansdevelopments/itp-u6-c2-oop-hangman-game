d163c6dcefd22bce25356173bb3cd9271acbcfea
Dan Martz <dansdevelopments@gmail.com>
2019-07-04 21:49:16 -0500

    Passing all tests

--- hangman/game.py
+++ hangman/game.py
@@ -54,6 +54,42 @@ class GuessWord(object):
         attempt = GuessAttempt(letter, hit=is_hit)
         return(attempt)
 
-
+import random
 class HangmanGame(object):
-    pass
+    WORD_LIST = ['rmotr','python','awesome']
+    def __init__(self, word_list=WORD_LIST, number_of_guesses=5):
+        self.word = GuessWord(self.select_random_word(word_list))
+        self.remaining_misses = number_of_guesses
+        self.previous_guesses = []
+        
+
+    @classmethod
+    def select_random_word(cls, word_list):
+        if type(word_list) != list:
+            raise InvalidListOfWordsException('word_list is of type {} and must be a list'.format(type(word_list)))
+        if len(word_list) == 0:
+            raise InvalidListOfWordsException("word_list can't be empty")
+        return random.choice(word_list)
+
+    def guess(self, letter):
+        if self.is_finished():
+            raise GameFinishedException
+        attempt = self.word.perform_attempt(letter)
+        if letter not in self.previous_guesses:
+            self.previous_guesses.append(letter.lower())
+        if attempt.is_miss():
+            self.remaining_misses -= 1
+        if self.is_won():
+            raise GameWonException
+        if self.is_lost():
+            raise GameLostException
+        return attempt
+
+    def is_finished(self):
+        return self.is_won() or self.is_lost()
+    
+    def is_lost(self):
+        return self.remaining_misses == 0
+
+    def is_won(self):
+        return self.word.answer == self.word.masked
